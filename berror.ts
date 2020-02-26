84471895e6454a1e7bfc11f61e7846a6657f3feb
Baptiste Marchand <baptiste.marchand@ambler.fr>
2020-02-26 23:51:48 +0100

    Add logging

--- berror.ts
+++ berror.ts
@@ -1,3 +1,27 @@
+import * as logMod from "https://deno.land/std/log/mod.ts";
+import { LogLevel } from "https://deno.land/std/log/levels.ts";
+import { Logger } from "https://deno.land/std/log/logger.ts";
+
+export type Level = "debug" | "verbose" | "info" | "warning" | "error"
+  | "critical";
+
+function levelToNumber(level: Level): number {
+  switch (level) {
+    case "debug":
+      return LogLevel.DEBUG;
+    case "verbose":
+      return LogLevel.VERBOSE;
+    case "info":
+      return LogLevel.INFO;
+    case "warning":
+      return LogLevel.WARNING;
+    case "error":
+      return LogLevel.ERROR;
+    case "critical":
+      return LogLevel.CRITICAL;
+  }
+}
+
 export class BError extends Error {
   constructor(
     msg: string,
@@ -15,5 +39,11 @@ export class BError extends Error {
       ...(cause instanceof BError ? cause.metadata : {}),
       ...metadata
     };
+  }
+
+  log({ level, logger }: { level?: Level; logger?: Logger} = {}) {
+    logger = logger ?? logMod.getLogger();
+    level = level ?? "error" 
+    logger._log(levelToNumber(level), this.message, this.metadata);
   }
 }
