cd7bc9ae53797d0320921900f37970754b12d346
Baptiste Marchand <baptiste.marchand@ambler.fr>
2020-02-27 21:32:48 +0100

    Remove semicolons

--- berror.ts
+++ berror.ts
@@ -4,12 +4,12 @@ export class BError extends Error {
     cause?: Error,
     public metadata?: Record<string, unknown>
   ) {
-    super(msg);
+    super(msg)
     if (cause != undefined) {
       const subMessage = cause instanceof Error
         ? cause.message
-        : `non-error object thrown: ${JSON.stringify(cause)}`;
-      this.message = `${this.message}: ${subMessage}`;
+        : `non-error object thrown: ${JSON.stringify(cause)}`
+      this.message = `${this.message}: ${subMessage}`
     }
     this.metadata = {
       ...(cause instanceof BError ? cause.metadata : {}),
