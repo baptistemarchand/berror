bb793aa6d16a1057d81441bcff6c70c4a31a4436
Baptiste Marchand <baptiste.marchand@ambler.fr>
2020-05-05 20:28:32 +0200

    deno: make tests compatible with deno 1.0

--- berror_test.ts
+++ berror_test.ts
@@ -1,33 +1,33 @@
 import { BError } from "./berror.ts";
 import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
 
-Deno.test(function onlyMesesage() {
+Deno.test("onlyMesesage", function () {
   const e = new BError("A");
   assertEquals(e.message, "A");
 });
 
-Deno.test(function messageAndCause() {
+Deno.test("messageAndCause", function () {
   const e = new BError("A", Error("B"));
   assertEquals(e.message, "A: B");
 });
 
-Deno.test(function full() {
+Deno.test("full", function () {
   const e = new BError("A", Error("B"), { foo: "bar" });
   assertEquals(e.message, "A: B");
 });
 
-Deno.test(function simpleChain() {
+Deno.test("simpleChain", function () {
   const e = new BError("A", new BError("B", new Error("C")));
   assertEquals(e.message, "A: B: C");
 });
 
-Deno.test(function metadata() {
+Deno.test("metadata", function () {
   const e1 = new BError("A", undefined, { foo: "bar", age: 42 });
   const e2 = new BError("A", e1, { foo: "baz", name: "Deno" });
   assertEquals(e2.metadata, { foo: "baz", age: 42, name: "Deno" });
 });
 
-Deno.test(function nonObject() {
+Deno.test("nonObject", function () {
   const e = new BError("A", ({ foo: "bar", age: 42 }) as any);
   assertEquals(
     e.message,
